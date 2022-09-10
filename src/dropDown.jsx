da18f0edaa0b38fa465e99f75cb535c66399eb7c
Julie Heckler <july.heckler@gmail.com>
2022-09-10 11:47:20 +0200

    comments

--- src/dropDown.jsx
+++ src/dropDown.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types'
@@ -10,11 +9,13 @@ function Dropdown({list, setValue, resetDrop}){
     const [show, setShow] = useState(false)
     const [selectedValue, setSelectedValue] = useState('- Select -')
 
+    //Reset the dropdown
     useEffect(()=>{
         setValue(selectedValue)
         if(resetDrop===true) {setSelectedValue('- Select -')}
     }, [resetDrop, selectedValue, setValue])
 
+    //The list of elements for the dropdown is pushed in an array 
     const datas=[]
 
     for (let i=0; i<list.length; i++){
@@ -25,7 +26,7 @@ function Dropdown({list, setValue, resetDrop}){
 
         datasDropdown.index = i
         datasDropdown.select = list[i]
-        
+
         datas.push(datasDropdown)
     }
 
@@ -33,6 +34,7 @@ function Dropdown({list, setValue, resetDrop}){
         return show? setShow(false) : setShow(true)
     }
 
+    //Render the value selected at the first line instead of '-Select-'
     function selectItem(el){
         setShow(false)
         setSelectedValue(el.select)
